f3a8d4dfd541dec4618a65350835d6b1e84a39d3
Onat <53895969+onattech@users.noreply.github.com>
2023-05-06 15:19:38 +0300

    Cleaned up sidebar links. Now fully functional.

---
+++ docs/4.operator-runtime/tutorials.md
@@ -0,0 +1,10 @@
+---
+slug: tutorials
+sidebar_class_name: "hide"
+---
+
+import DocCardList from '@theme/DocCardList';
+
+# Tutorials
+
+<DocCardList />
--- src/theme/DocSidebar/index.js
+++ src/theme/DocSidebar/index.js
@@ -4,17 +4,7 @@ import DocSidebar from "@theme-original/DocSidebar"
 export default function DocSidebarWrapper(props) {
     return (
         <>
-            <h3
-                style={{
-                    marginTop: "70px",
-                    paddingLeft: "20px",
-                    marginBottom: "-54px",
-                    borderBottom: "1px solid var(--ifm-toc-border-color)",
-                    paddingBottom: "10px",
-                }}
-            >
-                {prepareTitle(props.path)}
-            </h3>
+            <Title>{prepareTitle(props.path)}</Title>
             <DocSidebar {...props} />
         </>
     )
@@ -28,4 +18,21 @@ export default function DocSidebarWrapper(props) {
 function prepareTitle(path) {
     const titleArray = path.split("/")[2].split("-")
     return titleArray.map((a) => a[0].toUpperCase() + a.slice(1)).join(" ")
+}
+
+// ** Styled Components
+function Title({ children }) {
+    return (
+        <h3
+            style={{
+                marginTop: "70px",
+                paddingLeft: "20px",
+                marginBottom: "-54px",
+                borderBottom: "1px solid var(--ifm-toc-border-color)",
+                paddingBottom: "10px",
+            }}
+        >
+            {children}
+        </h3>
+    )
 }
