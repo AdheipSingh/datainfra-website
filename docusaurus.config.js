6c161fa44e750e0dc2231539af4cd5825007417a
AdheipSingh <adheip1222@gmail.com>
2023-05-07 23:30:08 +0530

    minor tweaks

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula")
 /** @type {import('@docusaurus/types').Config} */
 const config = {
     title: "DataInfra",
-    tagline: "Control Planes for Running Data Analytics",
+    tagline: "Control Planes for Real Time Analytics",
     favicon: "img/favicon.ico",
 
     // Set the production url of your site here
