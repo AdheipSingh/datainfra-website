e4650868d6282800d1e1f86a09badec7fcb8c909
Onat <53895969+onattech@users.noreply.github.com>
2023-05-11 22:40:38 +0300

    Custom terminal window WIP

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -2,6 +2,7 @@ import React from "react"
 // Import the original mapper
 import MDXComponents from "@theme-original/MDXComponents"
 import { Purple } from "@site/src/components/DocMainSection/index.jsx"
+import TerminalWindow from "@site/src/components/TerminalWindow/index.jsx"
 
 export default {
     // Re-use the default mapping
@@ -9,4 +10,5 @@ export default {
     // Map the "<Highlight>" tag to our Highlight component
     // `Highlight` will receive all props that were passed to `<Highlight>` in MDX
     Purple,
+    TerminalWindow,
 }
