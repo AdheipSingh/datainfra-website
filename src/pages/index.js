1f5cf8311b0143087ccb0b1afad75f4432f7af39
AdheipSingh <adheip1222@gmail.com>
2024-09-02 02:56:38 +0530

    fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import Layout from "@theme/Layout"
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext"
 import Link from "@docusaurus/Link"
 import styles from "./index.module.css"
-import "../css/v-main.css"
 
 export default function Home() {
     const { siteConfig } = useDocusaurusContext()
